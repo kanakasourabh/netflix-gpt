d9c1d1235969027a19e7580bcb7a0a93d9f7fbb5
Sourabh <kanakasourabh4@gmail.com>
2023-11-23 14:07:38 +0530

    cra & tailwng

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./App.css";
+function App() {
+  return (
+    <div className="text-3xl font-bold text-blue-100">
+      Namaste Everyone!! LEts rock n buld Netflix GPT
+    </div>
+  );
+}
+
+export default App;
